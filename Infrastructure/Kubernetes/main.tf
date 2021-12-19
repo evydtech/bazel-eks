fe8d10c378e9202f2eba7b63cec2617cc9bd428b
vish <vishesh.jaswl@gmail.com>
2021-12-19 16:28:26 +0800

    terraform edit

--- Infrastructure/Kubernetes/main.tf
+++ Infrastructure/Kubernetes/main.tf
@@ -61,7 +61,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   resource_group_name = azurerm_resource_group.k8s.name
   dns_prefix          = var.dns_prefix
 
-   linux_profile {
+  linux_profile {
      admin_username = "ubuntu"
   
      ssh_key {
